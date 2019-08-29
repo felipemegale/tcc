d24bd073a8ce79d308cdd02226e121d2d6d7bfea
Felipe Megale <felipe.megale17@gmail.com>
2019-08-28 21:00:31 -0300

    improve dist calc; add time diff calc

--- scripts/calcs.py
+++ scripts/calcs.py
@@ -1,4 +1,5 @@
 import math
+from datetime import datetime, timedelta
 
 
 def sin_rad(val):
@@ -11,5 +12,14 @@ def cos_rad(val):
 
 # this function returns the distance between the two given
 # geographical coordinates in kilometers
-def calculate_distance_between_positions(lat1, lon1, lat2, lon2):
-    return 6371 * math.acos(sin_rad(lat2)*sin_rad(lat1) + cos_rad(lat2)*cos_rad(lat1)*cos_rad(lon1-lon2))
+def calculate_distance_between_positions(ponto1, ponto2):
+    return 6371 * math.acos(sin_rad(ponto2[0])*sin_rad(ponto1[0]) + cos_rad(ponto2[0])*cos_rad(ponto1[0])*cos_rad(ponto1[1]-ponto2[1]))
+
+
+def calculate_time_difference_in_milliseconds(time1, time2):
+    datetime1 = datetime.strptime(time1, "%d/%m/%Y %H:%M")
+    datetime2 = datetime.strptime(time2, "%d/%m/%Y %H:%M")
+
+    diff = (datetime1 - datetime2)
+
+    return int(diff.total_seconds()*1000)
