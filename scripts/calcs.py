510dc727299f41013a4ac393c985f1095f69f27f
Felipe Marques Megale <fmegale@ibm.com>
2019-08-29 09:47:05 -0300

    rename distance calc function parameters

--- scripts/calcs.py
+++ scripts/calcs.py
@@ -12,8 +12,8 @@ def cos_rad(val):
 
 # this function returns the distance between the two given
 # geographical coordinates in kilometers
-def calculate_distance_between_positions(ponto1, ponto2):
-    return 6371 * math.acos(sin_rad(ponto2[0])*sin_rad(ponto1[0]) + cos_rad(ponto2[0])*cos_rad(ponto1[0])*cos_rad(ponto1[1]-ponto2[1]))
+def calculate_distance_between_positions(geo_loc_1, geo_loc_2):
+    return 6371 * math.acos(sin_rad(geo_loc_2[0])*sin_rad(geo_loc_1[0]) + cos_rad(geo_loc_2[0])*cos_rad(geo_loc_1[0])*cos_rad(geo_loc_1[1]-geo_loc_2[1]))
 
 
 def calculate_time_difference_in_milliseconds(time1, time2):
