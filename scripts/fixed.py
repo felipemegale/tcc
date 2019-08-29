91d7ce15adf5d29ac29cad5f333ebc84dc29150c
Felipe Marques Megale <fmegale@ibm.com>
2019-08-29 09:32:01 -0300

    rename fix numbers script

--- scripts/fixed.py
+++ scripts/fixed.py
@@ -7,14 +7,14 @@ def fix_number(number):
 
 fixed_list = []
 
-with open('uber_peru_2010_formatted_complete.csv') as file:
+with open('csv/uber_peru_2010_formatted_complete.csv') as file:
     reader = csv.reader(file, delimiter=';')
 
     for row in reader:
         fixed_list.append(
             [row[0], row[1], fix_number(row[2]), fix_number(row[3]), row[4], fix_number(row[5]), fix_number(row[6])])
 
-with open('uber_peru_2010_formatted_complete_fixed.csv', 'w') as out:
+with open('csv/uber_peru_2010_formatted_complete_fixed.csv', 'w') as out:
     writer = csv.writer(out, delimiter=';')
 
     for row in fixed_list:
