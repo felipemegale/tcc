9d1edb1e31f256960ddfcc97d95b3f990b4129b2
Felipe Marques Megale <fmegale@ibm.com>
2019-08-29 09:45:03 -0300

    included missing end_lon data column

--- scripts/fixed.py
+++ scripts/fixed.py
@@ -12,7 +12,7 @@ with open('csv/uber_peru_2010_formatted_complete.csv') as file:
 
     for row in reader:
         fixed_list.append(
-            [row[0], row[1], fix_number(row[2]), fix_number(row[3]), row[4], fix_number(row[5]), fix_number(row[6])])
+            [row[0], row[1], fix_number(row[2]), fix_number(row[3]), row[4], fix_number(row[5]), fix_number(row[6]), fix_number(row[7])])
 
 with open('csv/uber_peru_2010_formatted_complete_fixed.csv', 'w') as out:
     writer = csv.writer(out, delimiter=';')
