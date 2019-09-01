82df3b543abf7dbc960142fbb12b615cc487ff09
Felipe Megale <felipe.megale17@gmail.com>
2019-09-01 14:02:25 -0300

    comparison function improved
    also renamed functions to more succinct names

--- scripts/calcs.py
+++ scripts/calcs.py
@@ -4,13 +4,13 @@ from datetime import datetime, timedelta
 
 # this function returns the distance between the two given
 # geographical coordinates in kilometers
-def calculate_distance_between_positions(geoloc1, geoloc2):
+def calc_dist(geoloc1, geoloc2):
     if geoloc1[0] == geoloc2[0] and geoloc1[1] == geoloc2[1]:
         return 0.0
     return acos(sin(radians(geoloc2[0])) * sin(radians(geoloc1[0])) + cos(radians(geoloc2[0])) * cos(radians(geoloc1[0])) * cos(radians(geoloc1[1] - geoloc2[1]))) * 6371
 
 
-def calculate_time_difference_in_milliseconds(time1, time2):
+def calc_time_diff(time1, time2):
     datetime1 = datetime.strptime(time1, "%d/%m/%Y %H:%M")
     datetime2 = datetime.strptime(time2, "%d/%m/%Y %H:%M")
 
@@ -19,18 +19,31 @@ def calculate_time_difference_in_milliseconds(time1, time2):
     return int(diff.total_seconds()*1000)
 
 
-def compatible_by_origin(trip_a, trip_b):
-    start_time_trip_a = trip_a[2]
-    start_time_trip_b = trip_b[2]
+def compatible_by(or_dest, trip_a, trip_b):
+    if or_dest == "origin":
+        time_trip_a = trip_a[2]
+        time_trip_b = trip_b[2]
 
-    start_loc_trip_a = (trip_a[3], trip_a[4])
-    start_loc_trip_b = (trip_b[3], trip_b[4])
+        loc_trip_a = (float(trip_a[3]), float(trip_a[4]))
+        loc_trip_b = (float(trip_b[3]), float(trip_b[4]))
 
-    start_time_diff = calculate_time_difference_in_milliseconds(
-        start_time_trip_a, start_time_trip_b)
-    start_loc_dist = calculate_distance_between_positions(
-        start_loc_trip_a, start_loc_trip_b)
+    elif or_dest == "destination":
+        time_trip_a = trip_a[5]
+        time_trip_b = trip_b[5]
 
-    if start_time_diff <= 1200000 and start_loc_dist <= 1.5:
+        loc_trip_a = (trip_a[6], trip_a[7])
+        loc_trip_b = (trip_b[6], trip_b[7])
+
+    else:
+        return "You must choose 'origin' or 'destination' as comparison criterion"
+
+    time_diff = abs(calc_time_diff(
+        time_trip_a, time_trip_b))
+    loc_diff = abs(calc_dist(
+        loc_trip_a, loc_trip_b))
+
+    print(time_diff, loc_diff)
+
+    if time_diff <= 3600000 and loc_diff <= 1.5:
         return True
     return False
