7be3379b5a7e22e2b7737e44479170aa5de6b144
Felipe Megale <felipe.megale17@gmail.com>
2019-09-07 13:18:13 -0300

    change drawing specs

--- scripts/imagery.py
+++ scripts/imagery.py
@@ -8,7 +8,7 @@ files = os.listdir("graph_adjlists")
 
 for file_name in files:
     file_path = "graph_adjlists/{}".format(file_name)
-    img_name = "{}jpeg".format(file_name.split('adjlist')[0])
+    img_name = "{}png".format(file_name.split('adjlist')[0])
 
     print("{1} => reading file {0}".format(
         file_name, strftime("%Y-%m-%d %H:%M:%S", localtime())))
@@ -24,6 +24,6 @@ for file_name in files:
 
     print("{1} => started drawing image {0}".format(
         img_name, strftime("%Y-%m-%d %H:%M:%S", localtime())))
-    plt.savefig(img_name, quality=50, optimize=True)
+    plt.savefig(img_name, quality=50)
     print("{1} => finished drawing image {0}".format(
         img_name, strftime("%Y-%m-%d %H:%M:%S", localtime())))
