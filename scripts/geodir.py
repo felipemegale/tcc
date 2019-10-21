94fbe8fb57f731aac0a52e93a886f4b95a6ad52c
Felipe Megale <felipe.megale17@gmail.com>
2019-10-21 17:46:20 -0300

    update geodir

--- scripts/geodir.py
+++ scripts/geodir.py
@@ -1,5 +1,6 @@
-import requests
 import time
+import json
+import requests
 import pandas as pd
 
 url = "https://api.geodir.co/geocoder/geocoder-reverse/v1/findlatlon?y={0}&x={1}&token=b92005e0-814c-414e-8ac9-22c7d8c1ecf2"
@@ -13,16 +14,16 @@ ends_lat = trips['end_lat'].tolist()
 ends_lon = trips['end_lon'].tolist()
 ends_ubigeo = []
 
-for i in range(len(trips_id)):
-    start_req = requests.get(url.format(starts_lat[i], starts_lon[i]))
-    print((trips_id[i], start_req.json()))
-    time.sleep(5)
+with open("geodir_start.out", "w") as out:
+    for i in range(len(trips_id)):
+        start_req = requests.get(url.format(starts_lat[i], starts_lon[i]))
+        out.write(trips_id[i]+"\n")
+        out.write(json.dumps(start_req.json())+"\n")
+        time.sleep(3)
 
-for i in range(len(trips_id)):
-    end_req = requests.get(url.format(ends_lat[i], ends_lon[i]))
-    print((trips_id[i], end_req.json()))
-    time.sleep(5)
-
-print(starts_ubigeo)
-print("-----------")
-print(ends_ubigeo)
+with open("geodir_end.out", "w") as out:
+    for i in range(len(trips_id)):
+        end_req = requests.get(url.format(ends_lat[i], ends_lon[i]))
+        out.write(trips_id[i]+"\n")
+        out.write(json.dumps(end_req.json())+"\n")
+        time.sleep(3)
