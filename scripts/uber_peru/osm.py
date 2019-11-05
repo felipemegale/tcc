da2ec49e6e634c81477513b04ede6736c78ad8dd
Felipe Megale <felipe.megale17@gmail.com>
2019-11-05 08:00:25 -0300

    add osm requests

--- scripts/uber_peru/osm.py
+++ scripts/uber_peru/osm.py
@@ -1,10 +1,12 @@
 import requests
 import json
 import pandas as pd
+import json
+import time
 
 url = "https://nominatim.openstreetmap.org/reverse.php?lat={0}&lon={1}&format=json"
 
-trips = pd.read_csv("csv/uber_peru_ 2010_formatted_complete_fixed.csv", sep=';')
+trips = pd.read_csv("csv/uber_peru_2010_formatted_complete_fixed.csv", sep=';')
 trips_id = trips['journey_id'].tolist()
 starts_lat = trips['start_lat'].tolist()
 starts_lon = trips['start_lon'].tolist()
@@ -13,3 +15,16 @@ ends_lat = trips['end_lat'].tolist()
 ends_lon = trips['end_lon'].tolist()
 ends_district = []
 
+with open("osm_start.out", "w") as osm:
+    for i in range(len(trips_id)):
+        req = requests.get(url.format(starts_lat[i], starts_lon[i]))
+        osm.write(trips_id[i]+"\n")
+        osm.write(json.dumps(req.json())+"\n")
+        time.sleep(3)
+
+with open("osm_end.out", "w") as osm:
+    for i in range(len(trips_id)):
+        req = requests.get(url.format(ends_lat[i], ends_lon[i]))
+        osm.write(trips_id[i]+"\n")
+        osm.write(json.dumps(req.json())+"\n")
+        time.sleep(3)
