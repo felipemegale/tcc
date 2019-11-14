f92c252e7e4b83846e13b9a1447382a3817f41b7
Felipe Megale <felipe.megale17@gmail.com>
2019-11-14 11:48:29 -0300

    calculate rk1k2

--- scripts/uber_peru/od_matrix.py
+++ scripts/uber_peru/od_matrix.py
@@ -25,6 +25,8 @@ for dist in st_districts:
 districts.sort()
 # graph.add_nodes_from(districts)
 
+in_n_out = {district: {'in': 0, 'out': 0} for district in districts}
+
 matrix = [[0 for x in range(len(en_districts))]
           for y in range(len(st_districts))]
 
@@ -47,6 +49,8 @@ for i in range(len(st_jsons)):
         end = en_jsons[i]["address"]["village"]
 
     matrix[st_districts.index(start)][en_districts.index(end)] += 1
+    in_n_out[start]['out'] += 1
+    in_n_out[end]['in'] += 1
 
     # if graph.has_edge(start, end):
     #     graph[start][end]['weight'] = graph.get_edge_data(start, end)[
@@ -78,3 +82,20 @@ for i in range(len(prob_matrix[0])):
 # mas quem sao k1 e k2? no exemplo da aula são os graus dos vertices
 # só se eu converter o nome do distrito no indice dele na lista
 # mas ai distritos com Z ou perto do Z terao R(k1,k2) maiores... (ou menores)
+
+'''
+como eu vou pegar k1 e k2:
+k1 (coluna) eh o numero de arestas de saida
+k2 (linha) eh o numero de arestas de chegada
+'''
+
+rk1k2 = 0
+
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        k1 = in_n_out[en_districts[j]]['in']
+        k2 = in_n_out[st_districts[i]]['out']
+        pk1k2 = prob_matrix[st_districts.index(i)][en_districts.index(j)]
+        _pk1 = pk1[i]
+        _pk2 = pk2[j]
+        rk1k2 += k1*k2*(pk1k2 - _pk1*_pk2)
