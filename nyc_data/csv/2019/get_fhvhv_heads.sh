80d7bd7c69b2816c901a68849e439ba830fbc959
Felipe Megale <felipe.megale17@gmail.com>
2021-07-09 22:52:42 -0300

    add get fhvhv head scripts

---
+++ nyc_data/csv/2019/get_fhvhv_heads.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+head fhvhv_tripdata_2019-01.csv > head_fhvhv_tripdata_2019-01.csv
+head fhvhv_tripdata_2019-02.csv > head_fhvhv_tripdata_2019-02.csv
+head fhvhv_tripdata_2019-03.csv > head_fhvhv_tripdata_2019-03.csv
+head fhvhv_tripdata_2019-04.csv > head_fhvhv_tripdata_2019-04.csv
+head fhvhv_tripdata_2019-05.csv > head_fhvhv_tripdata_2019-05.csv
+head fhvhv_tripdata_2019-06.csv > head_fhvhv_tripdata_2019-06.csv
+head fhvhv_tripdata_2019-07.csv > head_fhvhv_tripdata_2019-07.csv
+head fhvhv_tripdata_2019-08.csv > head_fhvhv_tripdata_2019-08.csv
+head fhvhv_tripdata_2019-09.csv > head_fhvhv_tripdata_2019-09.csv
+head fhvhv_tripdata_2019-10.csv > head_fhvhv_tripdata_2019-10.csv
+head fhvhv_tripdata_2019-11.csv > head_fhvhv_tripdata_2019-11.csv
+head fhvhv_tripdata_2019-12.csv > head_fhvhv_tripdata_2019-12.csv
---
+++ nyc_data/csv/2020/get_fhvhv_heads.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+head fhvhv_tripdata_2020-01.csv > head_fhvhv_tripdata_2020-01.csv
+head fhvhv_tripdata_2020-02.csv > head_fhvhv_tripdata_2020-02.csv
+head fhvhv_tripdata_2020-03.csv > head_fhvhv_tripdata_2020-03.csv
+head fhvhv_tripdata_2020-04.csv > head_fhvhv_tripdata_2020-04.csv
+head fhvhv_tripdata_2020-05.csv > head_fhvhv_tripdata_2020-05.csv
+head fhvhv_tripdata_2020-06.csv > head_fhvhv_tripdata_2020-06.csv
+head fhvhv_tripdata_2020-07.csv > head_fhvhv_tripdata_2020-07.csv
+head fhvhv_tripdata_2020-08.csv > head_fhvhv_tripdata_2020-08.csv
+head fhvhv_tripdata_2020-09.csv > head_fhvhv_tripdata_2020-09.csv
+head fhvhv_tripdata_2020-10.csv > head_fhvhv_tripdata_2020-10.csv
+head fhvhv_tripdata_2020-11.csv > head_fhvhv_tripdata_2020-11.csv
+head fhvhv_tripdata_2020-12.csv > head_fhvhv_tripdata_2020-12.csv
