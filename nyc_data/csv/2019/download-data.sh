52df4249108608aeffee3ce2018cfb57269eafb5
Felipe Megale <felipe.megale17@gmail.com>
2021-07-10 23:31:16 -0300

    moved files from ssd to hdd

--- nyc_data/csv/2019/download-data.sh
+++ nyc_data/csv/2019/download-data.sh
@@ -1,62 +1,38 @@
 #!/bin/bash
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-01.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-01.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-01.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-01.csv
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-02.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-02.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-02.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-02.csv
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-03.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-03.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-03.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-03.csv
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-04.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-04.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-04.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-04.csv
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-05.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-05.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-05.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-05.csv
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-06.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-06.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-06.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-06.csv
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-07.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-07.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-07.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-07.csv
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-08.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-08.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-08.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-08.csv
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-09.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-09.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-09.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-09.csv
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-10.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-10.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-10.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-10.csv
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-11.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-11.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-11.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-11.csv
 
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/yellow_tripdata_2019-12.csv
-wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2019-12.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhv_tripdata_2019-12.csv
 wget https://nyc-tlc.s3.amazonaws.com/trip+data/fhvhv_tripdata_2019-12.csv
 
